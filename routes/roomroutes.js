c41e2cd6d57b1c216333ed743ad898997d0da01a
SUBHADIPDEYSuRvA0 <survasurva246@gmail.com>
2025-04-22 16:40:12 +0000

    push

--- routes/roomroutes.js
+++ routes/roomroutes.js
@@ -1,45 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const roomController = require('../controller/roomcontroller');
-const users = require('../models/user');
-const rooms = require('../models/room');
 
-// Create room route
 router.post('/create', roomController.createRoom);
-
-// Join room route
 router.post('/join', roomController.joinRoom);
 
-// Get room by code route
-router.get('/:code', async (req, res) => {
-  try {
-    const roomCode = req.params.code;
-
-    // Find the room by code
-    const room = await rooms.findOne({ code: roomCode });
-
-    if (!room) {
-      return res.status(404).send('Room not found');
-    }
-
-    // Find the user associated with the room (if any)
-    const user = req.session.user;  // Assuming user is stored in session
-
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-
-    // Render the call page with the room code and user info
-    res.render('call', { 
-      code: roomCode, 
-      user: user // Pass the user object to the view
-    });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
+router.get('/:code', (req, res) => {
+  res.render('call', { code: req.params.code });
 });
-
 
 module.exports = router;
